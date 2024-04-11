4112a4d081efd5a955271db3269f92a162afe59f
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2024-04-11 15:48:06 -0300

    leaderboard

--- parigolo-frontend/src/App.tsx
+++ parigolo-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from './components/Auth/Auth';
 import BetList from './components/BetList/BetList';
 import BetDetail from './components/BetDetail/BetDetail';
 import CreateRoom from './components/CreateRoom/CreateRoom';
+import Leaderboard from './components/Leaderboard/Leaderboard';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/bets" element={<BetList />} />
         <Route path="/bet/:id" element={<BetDetail />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
     </Router>
   );
